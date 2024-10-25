483e66a0dcc4ad512d772efaef0b3868ea5966d0
Jason <jdmarshall@users.noreply.github.com>
2024-10-24 17:50:40 -0700

    Calculate text during the event, not during render.

--- lib/weald_web/controllers/timer_live.ex
+++ lib/weald_web/controllers/timer_live.ex
@@ -2,37 +2,50 @@ defmodule WealdWeb.TimerLive do
   use WealdWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, %{seconds: 60, prompt: "Start"})}
+    {:ok, socket
+      |> assign(%{ prompt: "Start" })
+      |> setTime(25 * 60)}
+  end
+
+  def handle_event("start", _params, socket) do
+    {:noreply, startTimer(socket, 25 * 60)}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <%= @text %>
+    </div>
+    <div><button phx-click="start"><%= @prompt %></button></div>
+    """
   end
 
   def handle_info(:tick, socket) do
     if (socket.assigns.seconds <= 0) do
       :timer.cancel(socket.private.timer)
 
-      {:noreply, assign(socket, seconds: 0)}
+      {:noreply, setTime(socket, 0)}
     else
-      {:noreply, assign(socket, seconds: socket.assigns.seconds - 1)}
+      {:noreply, setTime(socket, socket.assigns.seconds - 1)}
     end
   end
 
-  def handle_event("start", _params, socket) do
+  def setTime(socket, time) do
+    text = Time.from_seconds_after_midnight(time)
+      |> Timex.format!("{0m}:{0s}")
+
+    assign(socket, %{seconds: time, text: text})
+  end
+
+  def startTimer(socket, time) do
     {ok, timer} = :timer.send_interval(1000, self(), :tick)
 
     if (socket.private[:timer]) do
       IO.inspect(:timer.cancel(socket.private.timer))
     end
 
-    socket = %{socket | private: Map.put(socket.private, :timer, timer)}
-
-    {:noreply, assign(socket, %{seconds: 60, prompt: "Restart"})}
-  end
-
-  def render(assigns) do
-    ~H"""
-    <div>
-      <%= Time.from_seconds_after_midnight(@seconds) |> Timex.format!("{0m}:{0s}") %>
-    </div>
-    <div><button phx-click="start"><%= @prompt %></button></div>
-    """
+    %{socket | private: Map.put(socket.private, :timer, timer)}
+      |> setTime(time)
+      |> assign(%{ prompt: "Restart" })
   end
 end
