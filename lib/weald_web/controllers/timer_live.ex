20875bb637d1debd4813f7a872de9750342bc237
Jason <jdmarshall@users.noreply.github.com>
2024-10-28 16:18:34 -0700

    Add some support for breaks.
    Remove the double-button for a simpler state machine.

--- lib/weald_web/controllers/timer_live.ex
+++ lib/weald_web/controllers/timer_live.ex
@@ -3,8 +3,9 @@ defmodule WealdWeb.TimerLive do
 
   def mount(_params, _session, socket) do
     {:ok, socket
-      |> assign(%{ startPrompt: "Start", pausePrompt: nil })
-      |> setTime(25 * 60)}
+      |> assign(%{ prompt: "Start", foo: "start" })
+      |> setTime(25 * 60)
+    }
   end
 
   def handle_event("start", _params, socket) do
@@ -17,21 +18,18 @@ defmodule WealdWeb.TimerLive do
 
   def render(assigns) do
     ~H"""
-    <div>
+    <div class="time text-9xl text-right">
       <%= @text %>
     </div>
-    <div>
-      <button phx-click="start"><%= @startPrompt %></button>
-      <button phx-click="pause" :if={@pausePrompt != nil}><%= @pausePrompt %></button>
+    <div class="text-right">
+      <button class="text-2xl" phx-click={@foo}><%= @prompt %></button>
     </div>
     """
   end
 
   def handle_info(:tick, socket) do
     if (socket.assigns.seconds <= 0) do
-      :timer.cancel(socket.private.timer)
-
-      {:noreply, setTime(socket, 0)}
+      {:noreply, finishTimer(socket)}
     else
       {:noreply, setTime(socket, socket.assigns.seconds - 1)}
     end
@@ -45,26 +43,39 @@ defmodule WealdWeb.TimerLive do
   end
 
   def startTimer(socket, time) do
-    {_ok, timer} = :timer.send_interval(1000, self(), :tick)
+    socket = cancelTimer(socket)
 
-    if (socket.private[:timer]) do
-      IO.inspect(:timer.cancel(socket.private.timer))
-    end
+    {_ok, timer} = :timer.send_interval(1000, self(), :tick)
 
     %{socket | private: Map.put(socket.private, :timer, timer)}
       |> setTime(time)
-      |> assign(%{ startPrompt: "Restart" })
-      |> assign(%{ pausePrompt: "Pause" })
+      |> assign(%{ prompt: "Pause" })
+      |> assign(%{ foo: "pause" })
+  end
+
+  def finishTimer(socket) do
+    cancelTimer(socket)
+      |> assign(%{ prompt: "Start", foo: "start" })
+      |> setTime(5 * 60)
   end
 
   def pauseResume(socket) do
     if (socket.private[:timer]) do
-      IO.inspect(:timer.cancel(socket.private.timer))
-
-      %{socket | private: Map.delete(socket.private, :timer)}
-        |> assign(%{ pausePrompt: "Continue" })
+      socket
+        |> cancelTimer()
+        |> assign(%{ prompt: "Resume", foo: "pause" })
     else
       startTimer(socket, socket.assigns.seconds)
     end
+  end
+
+  def cancelTimer(socket) do
+    if (socket.private[:timer]) do
+      :timer.cancel(socket.private.timer)
+
+      %{socket | private: Map.delete(socket.private, :timer)}
+    end
+
+    socket
   end
 end
